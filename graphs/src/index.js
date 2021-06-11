a754ccb4cbe216ca07b2f66b006f64d1a43604d1
Ayush Mahajan <ayush.mahajan@cred.club>
2021-06-11 13:15:58 +0530

    Wavelet Hashing

--- graphs/src/index.js
+++ graphs/src/index.js
@@ -4,6 +4,7 @@ import LineChart from './LineChart';
 const aHashData = require('./data/aHash.json');
 const dHashData = require('./data/dHash.json');
 const pHashData = require('./data/pHash.json');
+const wHashData = require('./data/whash.json');
 
 class App extends Component {
   
@@ -11,12 +12,25 @@ class App extends Component {
     super();
     
   }
+
+  runner(data,name){
+    return (<div>
+      <LineChart name={name + " Level 1"} data={{"Level 1" : aHashData["Level 1"]}}/>
+      <LineChart name={name + " Level 2"} data={{"Level 2" : aHashData["Level 2"]}}/>
+      <LineChart name={name + " Level 3"} data={{"Level 3" : aHashData["Level 3"]}}/>
+    </div>)
+  }
+
   render(){
     console.log(aHashData);
     return (<div>
-      <LineChart name="Average Hash" data={aHashData}/>
-      <LineChart name="Difference Hash" data={dHashData}/>
+      {this.runner(aHashData,"Average Hash")}
+      {this.runner(dHashData,"Difference Hash")}
+      {this.runner(pHashData,"Perceptual Hash")}
+      {this.runner(wHashData,"Wavelet Hash")}
+      {/* <LineChart name="Difference Hash" data={dHashData}/>
       <LineChart name="Perceptual Hash" data={pHashData}/>
+      <LineChart name="Wavelet Hash" data={wHashData}/> */}
     </div>)
   }
 }
--- wHash.py
+++ wHash.py
@@ -7,7 +7,10 @@ from PIL import Image, ImageFilter,UnidentifiedImageError
 import random
 import art
 import json
-output_stream = open("./graphs/src/data/aHash.json","w")
+import numpy
+import scipy.fftpack
+import pywt
+output_stream = open("./graphs/src/data/whash.json","w")
 
 print(art.text2art("aHash"))
 
@@ -58,25 +61,44 @@ def calculateMatch(a,b):
     return round((BITS-hammingDistance(a,b))/BITS*100,2)
 
 
-def ahash(image):
+def whash(image):
     hash_size = 8
+    image_natural_scale = 2**int(numpy.log2(min(image.size)))
+    image_scale = max(image_natural_scale, hash_size)
+    ll_max_level = int(numpy.log2(image_scale))
+    level = int(numpy.log2(hash_size))
+    dwt_level = ll_max_level - level
     # Grayscale and shrink the image in one step.
     image = image.convert('L').resize(
-        (hash_size + 1, hash_size),
+        (image_scale,image_scale),
         Image.ANTIALIAS,
     )
-    pixels = list(image.getdata())
-    average = 0
-    for row in range(hash_size):
-        for col in range(hash_size):
-            average += image.getpixel((col,row))
-    average = average/(hash_size**2)
-    # Compare adjacent pixels.
+    pixels = []
+    for row in range(image_scale):
+        cur_row = []
+        for col in range(image_scale):
+            cur_row.append(image.getpixel((col,row))/255)
+        pixels.append(cur_row)
+    coeffs = pywt.wavedec2(pixels, 'haar', level = ll_max_level)
+    coeffs = list(coeffs)
+    coeffs[0] *= 0
+    pixels = pywt.waverec2(coeffs, 'haar')
+    # print("pixel",pixels)
+    
+    coeffs = pywt.wavedec2(pixels, 'haar', level = dwt_level)
+    dwt_low = coeffs[0]
+    median = numpy.median(dwt_low)
+
+    # print("pixel",len(pixels),"X",len(pixels[0]));
+    # print("coeff",len(coeffs),"X",len(coeffs[0]));
+    # print("median",median)
+    # print("dwt",len(dwt_low),"X",len(dwt_low[0]));
     difference = []
+    # print("dwt low",dwt_low)
     for row in range(hash_size):
         for col in range(hash_size):
-            current_pixel = image.getpixel((col,row))
-            difference.append(current_pixel >= average)
+            current_pixel = dwt_low[row][col]
+            difference.append(current_pixel >= median)
     # Convert the binary array to a hexadecimal string.
     decimal_value = 0
     hex_string = []
@@ -102,8 +124,9 @@ for file in files:
         image = Image.open(path)
         data.append({
             "path" : file,
-            "hash" : int(ahash(image),16)
+            "hash" : int(whash(image),16)
         })
+        # exit(0)
     except UnidentifiedImageError:
         pass
 imageLoadingLoader.removeLoader()
@@ -111,12 +134,16 @@ imageLoadingLoader.removeLoader()
 all_match = {}
 result = {}
 row = len(data)
+
+# output_stream.write(json.dumps(data))
+# exit(0)
 imageProcessingLoader = Loader("Image Processing",row*(row-1))
 for i in range(row):
     for j in range(i+1,row):
         imageProcessingLoader.printLoader(i*row+j+1)
         lev = diff(data[i]["path"],data[j]["path"])
         match = calculateMatch(data[i]["hash"],data[j]["hash"])
+        # print(lev,match,data[i]["path"],data[j]["path"])
         if lev not in result:
             result[lev] = {}
         if match not in result[lev]:
@@ -126,6 +153,7 @@ for i in range(row):
             all_match[match] = 1
 
 imageProcessingLoader.removeLoader()
+
 
 for lev in result:
     for match in all_match:
@@ -141,4 +169,3 @@ for lev in result:
     result[lev] = sorted(data)
 
 output_stream.write(json.dumps(result))
-
